a3836e560fd5abe452a378e745e06bd4bd69d3d0
王佳淳 <1609303379@qq.com>
2022-08-30 18:06:32 +0800

    每日1题：998 数组 ：645

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from idlelib.tree import TreeNode
+
+
 class data_structure_exercise(object):
     def findMaxConsecutiveOnes_485(self):
         nums = [1, 1, 0, 1,0,1,1,1]
@@ -62,8 +65,36 @@ class data_structure_exercise(object):
         # print(frist_max)
         # print(sencond_max)
         # print(thirdMax)
-        return thirdMax
+        # return thirdMax
 
+
+
+    def maximumProduct_628(self):
+        nums = [-4,-6,-3,4,9,5]
+        nums.sort(reverse = True)
+        n =len(nums)
+        print(nums,n)
+        manimum = max(nums[0]*nums[1]*nums[2],nums[0]*nums[n-2]*nums[n-1],)
+        print(manimum)
+        return manimum
+
+    def findErrorNums_645(self, nums):
+        res = 0
+        length = len(nums)
+        err = sum(nums) - sum(set(nums))  # 重复
+        for n in nums:
+            res ^= n
+        for i in range(1, length + 1):
+            res ^= i
+        rep = err ^ res
+        result= [err, rep]
+        print(result)
+        return result
+
+
+
+
+class everday(object):
     def shuffle_1470(self):
         '''
         注释是参考答案
@@ -81,13 +112,46 @@ class data_structure_exercise(object):
         # print(nums_list)
         return  ans
 
-
-
+    def insertIntoMaxTree_998(self):
+        root =[4, 1, 3, None, None, 2],
+        val =5
+        '''
+        有两种解题方法
+        '''
+        '''
+        官方方法
+        '''
+        # parent, cur = None, root
+        # while cur:
+        #     if val > cur.val:
+        #         if not parent:
+        #             return TreeNode(val, root, None)
+        #         node = TreeNode(val, cur, None)
+        #         parent.right = node
+        #         return root
+        #     else:
+        #         parent = cur
+        #         cur = cur.right
+        #
+        # parent.right = TreeNode(val)
+        # return root
+        '''
+        个人方法
+        '''
+        if root == None: return TreeNode(val)
+        if val > root.val:
+            return TreeNode(val, root, None)
+        root.right = self.insertIntoMaxTree(root.right, val)
+        return root
 
 
 if __name__ == "__main__":
     main_class=data_structure_exercise()
+    everday =everday()
     # q485 = main_class.findMaxConsecutiveOnes_485()
     # q495 = main_class.findPoisonedDuration_495()
     # q1470= main_class.shuffle_1470()
     # q414 = main_class.thirdMax_414()
+    # q628 =main_class.maximumProduct_628()
+    # q645 = main_class.findErrorNums_645([5,3,4])
+    q998 = everday.insertIntoMaxTree_998()
